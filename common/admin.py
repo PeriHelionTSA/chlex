a0bf5c0112af98e559ee9c790798405859e9231c
David Höppner <0xffea@gmail.com>
2014-05-17 19:32:35 +0200

    Use TabularInline to edit meanings in lems

--- common/admin.py
+++ common/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
 from common.models import Lem
+from common.models import Meaning
 
+
+class MeaningInline(admin.TabularInline):
+    model = Meaning
 
 class LemAdmin(admin.ModelAdmin):
-    pass
+    inlines = [
+        MeaningInline,
+    ]
 
 admin.site.register(Lem, LemAdmin)
--- common/models.py
+++ common/models.py
@@ -5,7 +5,6 @@ class Meaning(models.Model):
     meaning = models.CharField(max_length=5000)
     lem = models.ForeignKey("Lem")
 
-# Create your models here.
 class Lem(models.Model):
     graphs = models.CharField(max_length=5000)
     pinyin = models.CharField(max_length=5000)
