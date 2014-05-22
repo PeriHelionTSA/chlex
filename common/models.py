0a8d1b2056e52979c960f637a31105fcf622b266
UMiddendorf <ulrikemiddendorf@web.de>
2014-05-22 12:53:43 +0200

    unicode method for Lem

--- common/models.py
+++ common/models.py
@@ -8,3 +8,6 @@ class Meaning(models.Model):
 class Lem(models.Model):
     graphs = models.CharField(max_length=5000)
     pinyin = models.CharField(max_length=5000)
+
+    def __unicode__(self):
+        return self.graphs + u" " + self.pinyin
