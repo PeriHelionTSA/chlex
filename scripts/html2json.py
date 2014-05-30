9555d8d5f3ca6716511a0ae4602747e167bbc90c
David Höppner <0xffea@gmail.com>
2014-05-30 19:32:00 +0200

    Better version of HTML parser

--- scripts/html2json.py
+++ scripts/html2json.py
@@ -6,12 +6,13 @@ from lxml import etree
 parser = etree.HTMLParser()
 html = etree.parse('lemmata.html', parser)
 
-rows = html.xpath('//tr')
+rows = html.xpath('//table')[0].findall('tr')
 
 for row in rows:
-    for idx, col in enumerate(row.xpath('//td')):
-        print col.text
-  
-        if idx % 3: 
-            for meaning in col.xpath('ol//li'):
-                print meaning.text
+    cols = row.findall('td')
+    if len(cols) == 4:
+        print cols[0].text, cols[1].text
+        meaning = [c.text for c in cols[3].findall('ol/li')]
+        print meaning
+        print "\n\n"
+
