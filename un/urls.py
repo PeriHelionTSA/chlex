61d671458bde538c13ebfc9d1ffc1e5b3e312fcb
UMiddendorf <ulrikemiddendorf@web.de>
2014-07-10 11:47:02 +0200

    two view functions

--- un/urls.py
+++ un/urls.py
@@ -1,9 +1,16 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include
+from django.conf.urls import patterns
+from django.conf.urls import url
 from django.contrib import admin
+
+from un.views import about
+from un.views import contact
 from un.views import index
 
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^about', about),
+    url(r'^contact', contact),
     url(r'^$', index), 
 )
--- un/views.py
+++ un/views.py
@@ -7,3 +7,9 @@ def index(request):
     qs = Lem.objects.all()
     qs = qs.order_by('pinyin')
     return render(request, 'un/index.html', {'lemmata': qs})
+
+def about(request):
+    return render(request, 'un/about.html')
+
+def contact(request):
+    return render(request, 'un/contact.html')
